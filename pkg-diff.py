517430a5a0bfb76ce2041f95201ecce143956288
Colin Watson <colin.watson@canonical.com>
2004-10-25 16:03:18 +0000

    Apply copyright notice and GPL (OKed by Mark).

--- pkg-diff.py
+++ pkg-diff.py
@@ -2,6 +2,25 @@
 # -*- coding: UTF-8 -*-
 # arch-tag:
 
+# Copyright (c) 2004 Canonical Ltd.
+#
+# This file is part of Germinate.
+#
+# Germinate is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# Germinate is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Germinate; see the file COPYING.  If not, write to the Free
+# Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
 import os,sys,getopt
 
 class Package:
